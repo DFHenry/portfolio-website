f94d6a72b50090ef14cdbcf597e87ddaa0be0c37
David Henry <david_freeman_henry@hotmail.com>
2026-03-02 09:44:21 -0500

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,18 @@
+//javascript for smartphone menu button toggle
+var menuOn = false;
+
+function toggleMenu()
+{
+    var menu = document.getElementById("main-menu");
+
+    if(menuOn == false)
+    {
+        menu.style.display = "block";
+        menuOn = true;
+    }
+    else
+    {
+        menu.style.display = "none";
+        menuOn = false;
+    }
+}
