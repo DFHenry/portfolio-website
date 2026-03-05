548dcca00434168010c4beabd6fde94b47ab6d1b
David Henry <david_freeman_henry@hotmail.com>
2026-03-05 17:35:15 -0500

    completed website for initial deployment

--- js/java.js
+++ js/java.js
@@ -1,10 +1,12 @@
 //javascript for smartphone menu button toggle
 var menuOn = false;
 
+
+
 function toggleMenu()
 {
     var menu = document.getElementById("main-menu");
-
+    
     if(menuOn == false)
     {
         menu.style.display = "block";
@@ -15,4 +17,5 @@ function toggleMenu()
         menu.style.display = "none";
         menuOn = false;
     }
+    return false;
 }
